364651117e24023914909c2e0a4b39abc5bdfbd3
g25ai1058 <g25ai1058@iitj.ac.in>
2026-06-07 15:50:25 +0000

    modified file

--- src/byzantine_faults/crypto_utils.py
+++ src/byzantine_faults/crypto_utils.py
@@ -1,9 +1,37 @@
-from crypto_utils import sign_message, verify_message
-message = "TX001 : Pay Rs 100"
-signature = sign_message(message)
-print("Message:", message)
-print("Signature Generated")
-if verify_message(message, signature):
-print("Signature Verified")
-else:
-print("Verification Failed")
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+
+# Generate key pair
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+
+public_key = private_key.public_key()
+
+
+# Sign message
+def sign_message(message):
+    signature = private_key.sign(
+        message.encode(),
+        padding.PKCS1v15(),
+        hashes.SHA256()
+    )
+
+    return signature
+
+
+# Verify signature
+def verify_message(message, signature):
+    try:
+        public_key.verify(
+            signature,
+            message.encode(),
+            padding.PKCS1v15(),
+            hashes.SHA256()
+        )
+
+        return True
+
+    except Exception:
+        return False
